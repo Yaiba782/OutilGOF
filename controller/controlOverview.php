e855df3c68cc146d82b64bf1d9be4b664c183023
Yaiba <yaiba782@gmail.com>
2016-02-03 16:41:18 +0100

    Trying to keep the commit strike ! :D

---
+++ controller/controlOverview.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Yaiba.
+ * Date: 02/02/16
+ * Time: 11:14
+ */
+
+    include_once(__DIR__.'/../model/modelsLoader.php');
+    include_once(__DIR__.'/../includes/includes.php');
+
+    function afficherMRParFlotte($listeMR){
+
+    }
