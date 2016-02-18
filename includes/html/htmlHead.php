97f6186ec9b4af5a274633b273281161cef556a5
Yaiba <yaiba782@gmail.com>
2016-02-18 13:58:40 +0100

    Working on the new web version

--- includes/html/htmlHead.php
+++ includes/html/htmlHead.php
@@ -9,6 +9,7 @@
             <head>
                 <link href="'.dirname('__FILE__').'/../includes/css/bootstrap.min.css" rel="stylesheet">
                 <link href="'.dirname('__FILE__').'/../includes/css/style.css" rel="stylesheet">
+                <link href="'.dirname('__FILE__').'/../includes/css/font-awe/css/font-awesome.min.css" rel="stylesheet">
                 <meta charset="UTF-8"/>
             </head>
             <body>';
