fb2b498eb01ebfb0f2c027aa138ddb0b8a0c1efc
Yaiba <yaiba782@gmail.com>
2016-04-07 16:32:03 +0200

    Changing the table select

--- tests/taleau.php
+++ tests/taleau.php
@@ -6,7 +6,9 @@
     <style>.A{background-color: blue;} .B{background-color: orange;} .C{background-color: green;} .D{background-color: red;}</style>
 </head>
 <body>
-    <button id="copyButton" data-clipboard-target="tableau">Copier le tableau</button>
+
+<input type="button" value="select table"
+       onclick="selectElementContents( document.getElementById('tableau') );">
 <table id="tableau">
 <tr>
     <th>1</th>
@@ -52,9 +54,27 @@
         <td class="D">D</td>
     </tr>
 </table>
-<script src="../includes/zeroclipboard/dist/ZeroClipboard.min.js"></script>
 <script>
-    var copiedTable = new ZeroClipboard( document.getElementById("copyButton") );
+    function selectElementContents(el) {
+        var body = document.body, range, sel;
+        if (document.createRange && window.getSelection) {
+            range = document.createRange();
+            sel = window.getSelection();
+            sel.removeAllRanges();
+            try {
+                range.selectNodeContents(el);
+                sel.addRange(range);
+            } catch (e) {
+                range.selectNode(el);
+                sel.addRange(range);
+            }
+        } else if (body.createTextRange) {
+            range = body.createTextRange();
+            range.moveToElementText(el);
+            range.select();
+        }
+    }
+
 
 </script>
 </body>
