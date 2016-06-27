429ece936757e1ec097870dc9a0e1fa78f356597
Yaiba <yaiba782@gmail.com>
2016-06-27 11:19:41 +0200

    tests

--- tests/boucleGet.php
+++ tests/boucleGet.php
@@ -4,11 +4,11 @@
      * Date: 26/06/2016
      * Time: 16:08
      */
-    include('./testSR.php');
+    include('../includes/connexion.php');
     $opt = array( 'http' =>
         array(
             'method' => 'GET',
-            'header' => 'Authorization: Basic FjiezofGzhFKEZfezp= '
+            'header' => 'Authorization: Basic '.base64_encode('9503915T:Sup4sswd').' '
         )
     );
     $context = stream_context_create($opt);
@@ -20,15 +20,34 @@
     $listeSR = $obligatoire->fetchAll(PDO::FETCH_ASSOC);
 
     $time = time();
-    while (time()<$time+30){
-        foreach($listeSR['diminutif_site_realisateur'] as $sr){
-            $url = "http://x64lmwbiix2.lamulatiere.dsit.sncf.fr:11280/OsmoBoard/managementVisuel/getData/".$sr;
+    #while (time()<$time+30){
+        foreach($listeSR as $sr){
+            $url = "http://x64lmwbiix2.lamulatiere.dsit.sncf.fr:11280/OsmoBoard/managementVisuel/getData/".$sr['diminutif_site_realisateur'];
+            $apiAnswer = file_get_contents($url,false,$context);
 
-            $result = file_get_contents($url,false,$context);
+            $apiJson = json_decode($apiAnswer);
+            $apiArray =get_object_vars($apiJson);
+
+            //On récup toutes les DI sans RDV dans un tableau
+            $diSansRdvArray = $apiArray['orphanInterventions'];
+
+            // On récup les RDV et les DI liées
+            $rdvArray = get_object_vars($apiArray['rdvs']);
+            foreach($rdvArray as $rdv){
+                $rdv = get_object_vars($rdv);
+                var_dump($rdv);
+                $rdvObject = new rdv($rdv['id'],apiTime($rdv['startDate']),apiTime($rdv['endDate']),null,null,null,null);
+                foreach ($rdv['interventions'] as $intervention){
+                    $diArray = get_object_vars($intervention);
+                    $di = new intervention($diArray['id'],materiel::findIdByNumero($diArray['efNumberMaterielRoulant'],$GLOBALS['connexion']),$diArray['label'],null,$diArray['status'],$diArray['codeOpération'],$rdvObject->getDateDebutRdv(),$rdvObject->getDateFinRdv(),apiTime($diArray['estimatedStartDate']),apiTime($diArray['estimatedEndDate']),$diArray['realEndDate'],$diArray['site'],null, null, null);
+                }
+            }
+
+
         }
         // On va chercher les SR de priorite 2 et 3
         $priorite = "SELECT diminutif_site_realisateur FROM site_realisateur WHERE priorite = 2";
-        $paspriorite = "SELECT diminutif_site_realisateur FROM site_realisateur WHERE priorite = 3";
+        $paspriorite = "SELECT diminutif_site_realisateur FROM site_realisateur WHERE priorite = 3 AND checked = 0";
 
         $priorite = $GLOBALS['connexion']->prepare($priorite);
         $priorite->execute();
@@ -40,4 +59,4 @@
         $listePriorite = $priorite->fetchAll(PDO::FETCH_ASSOC);
         $listePasPriorite = $paspriorite->fetchAll(PDO::FETCH_ASSOC);
 
-    }
+    #}
